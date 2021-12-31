9d55bc3d6b245d94cffbf3440a39798ec3fa5aa6
Gokul A <prasanthgokulgp@gmail.com>
2021-12-31 09:38:06 +0530

    Initial setup

---
+++ XUrl/src/main/java/com/xurl/XUrlApplication.java
@@ -0,0 +1,13 @@
+package com.xurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XUrlApplication.class, args);
+	}
+
+}
