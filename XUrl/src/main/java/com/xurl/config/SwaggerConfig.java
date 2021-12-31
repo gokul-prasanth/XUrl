602aef5cee6d30599bf1d86b57fd4b7c01190574
Gokul A <prasanthgokulgp@gmail.com>
2021-12-31 16:33:24 +0530

    apis for crud

--- XUrl/src/main/java/com/xurl/config/SwaggerConfig.java
+++ XUrl/src/main/java/com/xurl/config/SwaggerConfig.java
@@ -1,9 +1,17 @@
 package com.xurl.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -16,9 +24,27 @@ public class SwaggerConfig {
 		Docket docket = new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.xurl"))
-				.build();
+				.build()
+				.apiInfo(metaInfo())
+				.globalRequestParameters(globalImplicitParam());
 
 		return docket;
+	}
+
+	private List<RequestParameter> globalImplicitParam() {
+		var userParam = new RequestParameterBuilder().name("USER")
+				.description("User's Name")
+				.query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)).defaultValue("SYSTEM"))
+				.in(ParameterType.HEADER).required(true).build();
+		return List.of(userParam);
+	}
+
+	private ApiInfo metaInfo() {
+		return new ApiInfoBuilder()
+				.title("Xurl Apis")
+				.description("This is a API documentation page for XUrl Application APIs")
+				.version("1.0.0")
+				.build();
 	}
 
 }
--- XUrl/src/main/java/com/xurl/controller/XUrlController.java
+++ XUrl/src/main/java/com/xurl/controller/XUrlController.java
@@ -1,6 +1,7 @@
 package com.xurl.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +21,44 @@ public class XUrlController {
 	@Autowired
 	private XUrl xurl;
 
-	@PostMapping("/register")
+	@PostMapping("/register/{longUrl}")
 	@ApiImplicitParam(name = "USER", required = true, dataTypeClass = String.class, paramType = "header")
-	@ApiOperation("Get a short url for a long url.")
-	public String viewHomePage(@PathVariable("longUrl") String longUrl) {
+	@ApiOperation("Register and get the short url for the given long url.")
+	public String registerUrl(@PathVariable("longUrl") String longUrl) {
 		String shortUrl = xurl.registerNewUrl(longUrl);
 		return shortUrl;
 	}
+
+	@PostMapping("/register/{longUrl}/{shortUrl}")
+	@ApiImplicitParam(name = "USER", required = true, dataTypeClass = String.class, paramType = "header")
+	@ApiOperation("Replace the short url for the given long url with new short url.")
+	public String replaceUrl(@PathVariable("longUrl") String longUrl, @PathVariable("shortUrl") String shortUrl) {
+		String newShortUrl = xurl.registerNewUrl(longUrl, shortUrl);
+		return newShortUrl;
+	}
+
+	@GetMapping("/getLongUrl/{shortUrl}")
+	@ApiImplicitParam(name = "USER", required = true, dataTypeClass = String.class, paramType = "header")
+	@ApiOperation("Get the long url for the given short url.")
+	public String getUrl(@PathVariable("shortUrl") String shortUrl) {
+		String longUrl = xurl.registerNewUrl(shortUrl);
+		return longUrl;
+	}
+
+	@PostMapping("/delete/{longUrl}")
+	@ApiImplicitParam(name = "USER", required = true, dataTypeClass = String.class, paramType = "header")
+	@ApiOperation("Delete both short and long url for given long url.")
+	public String deleteUrl(@PathVariable("longUrl") String longUrl) {
+		String shortUrl = xurl.delete(longUrl);
+		return shortUrl;
+	}
+
+	@GetMapping("/getCount/{longUrl}")
+	@ApiImplicitParam(name = "USER", required = true, dataTypeClass = String.class, paramType = "header")
+	@ApiOperation("Get the hit count for given long url.")
+	public Integer getCount(@PathVariable("longUrl") String longUrl) {
+		Integer count = xurl.getHitCount(longUrl);
+		return count;
+	}
+	
 }
